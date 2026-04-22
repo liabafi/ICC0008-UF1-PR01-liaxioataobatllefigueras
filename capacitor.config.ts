9d03a42d4df7e307fbef9c69172995f77211a976
liabafi <lbatllefigueras@gmail.com>
2026-04-22 19:39:39 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ICC0008-UF1-PR01-liaxioataobatllefigueras',
+  webDir: 'www'
+};
+
+export default config;
